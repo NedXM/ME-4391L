b666b50fc19621d4406c4ccf3cc8a11212497b92
2ggoulenko <143643949+2ggoulenko@users.noreply.github.com>
2023-09-05 14:20:08 -0700

    Add files via upload

--- Goulenko1.m
+++ Goulenko1.m
@@ -12,7 +12,48 @@ for x=1:8
        end
     end
 end
+
 %% Problem  2
 t=0:0.01:2*pi;
 f=(sin(5*t))/4;
-g=exp(-1*t).*cos(10*t)
+g=exp(-1*t).*cos(10*t);
+
+plot (t, f);
+
+hold on;
+
+plot (t, g);
+
+xlim([0 6]);
+
+ylim([-1 1])
+
+title ('Plots of f(t) and g(t)')
+
+xlabel('Time (s)')
+
+ylabel('f(t), g(t)')
+
+legend( 'f(t)', 'g(t)');
+
+grid on;
+
+%% Problem 3
+
+% Create a transfer function model for the variable "s3"
+
+s3 = tf('s');
+
+p3 = 10/(s3^2+3*s3+10);
+
+% Compute impulse response
+
+[y, t] = impulse (p3, 5);
+
+% Plot impulse response
+
+plot (t,y);
+
+impulse(s3,t)
+
+
